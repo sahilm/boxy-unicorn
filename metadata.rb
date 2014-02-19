18f63791a45186de43ef36dd114f14bf76c408f3
Michael Nikitochkin <miry.sof@gmail.com>
2014-02-19 17:58:41 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'miry.sof@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures unicorn'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends          'rbenv'
 depends          'rails-stack'
